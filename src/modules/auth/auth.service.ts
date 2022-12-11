a7811c651fe21817383927b9c911e7353a5c8292
Alexandre Koch Teodoreto <alekteodoreto@hotmail.com>
2022-12-11 19:46:48 -0300

    feat(Auth): Validate user credentials

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,4 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
+import { UserService } from '../user/user.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private readonly userService: UserService) {}
+
+  async validateUser(email: string, password: string) {
+    const user = await this.userService.findByEmail(email);
+
+    if (user) {
+      const passwordValid = await bcrypt.compare(password, user.password);
+
+      if (passwordValid) {
+        return {
+          ...user,
+          password: undefined,
+        };
+      }
+    }
+
+    throw new Error('Invalid credentials');
+  }
+}
