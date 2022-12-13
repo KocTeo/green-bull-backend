705ec4546498d2a68147590a95ce6cf5184691bc
Alexandre Koch Teodoreto <alekteodoreto@hotmail.com>
2022-12-13 18:52:22 -0300

    refactor(Payment): Remove Payment Model

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,14 +2,13 @@ import { Module } from '@nestjs/common';
 import { APP_GUARD } from '@nestjs/core';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AddressModule } from './modules/address/address.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { JwtAuthGuard } from './modules/auth/guards/jwt-auth.guard';
-import { UserModule } from './modules/user/user.module';
 import { CategoryModule } from './modules/category/category.module';
-import { AddressModule } from './modules/address/address.module';
-import { PaymentModule } from './modules/payment/payment.module';
 import { ProductModule } from './modules/product/product.module';
 import { SaleModule } from './modules/sale/sale.module';
+import { UserModule } from './modules/user/user.module';
 
 @Module({
   imports: [
@@ -17,7 +16,6 @@ import { SaleModule } from './modules/sale/sale.module';
     AuthModule,
     CategoryModule,
     AddressModule,
-    PaymentModule,
     ProductModule,
     SaleModule,
   ],
